a905070160a810fc59550571d71b90f97c6300e7
Dwarikanath <ddwarikanath15@gmail.com>
2021-10-11 20:40:07 +0530

    varialbles Operators

---
+++ 02Basics/01variables.js
@@ -0,0 +1,17 @@
+var fullName = "Dwarikanath";
+
+var courseName = "ReactJs Bootcamp";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+
+var paymentMode;
+
+paymentMode = "Credit card"
+console.log(paymentMode);
+
---
+++ 02Basics/02variable-two.js
@@ -0,0 +1,22 @@
+const uid = "abc123";
+
+var fullName = "Dwarikanath";
+var email = "dnrocks15@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fullName = prompt("Enter your name: "); attach to webPage
+
+console.log("Full name is : " + fullName);
+console.log(email);
+console.log(uid);
+console.log(courseCount);
+
+console.log(`
+    with unique id: ${uid}
+    User is: ${fullName}
+    and his email is : ${email}
+    and he uses the password:   ${password}
+`);
